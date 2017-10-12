fdc18921e3d554df6efe3f6acbce4460db7a16f0
Fábio Rogerio <fabopereira@gmail.com>
2017-10-12 18:59:40 -0300

    Added "Add Location" screen component

---
+++ src/components/AddLocation.js
@@ -0,0 +1,20 @@
+/**
+ * SkyWatch
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Text,
+  View
+} from 'react-native';
+
+export default class AddLocation extends Component {
+  render() {
+    return (
+      <View>
+        <Text>AddLocation</Text>
+      </View>
+    );
+  }
+}
