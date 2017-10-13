a74cb4bf60daa58ce0ee2d4ff8f8250dc78b3e7d
Fábio Rogerio <fabopereira@gmail.com>
2017-10-13 00:24:56 -0300

    Update home screen to handle error and show basic info about current location

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -6,10 +6,30 @@
 import React, { Component } from 'react';
 import {
   Text,
-  View
+  View,
+  Image
 } from 'react-native';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as locationsActions from '../actions/locations';
 
-export default class HomeScreen extends Component {
+const mapStateToProps = (state, ownProps) => {
+  return {
+    defaultLocation: state.locations.locations.find((l) => l.default),
+    otherLocations: state.locations.locations.filter((l) => !l.default),
+    shouldRequestForGeolocationPermission: state.locations.shouldRequestForGeolocationPermission,
+    defaultLocationError: state.locations.defaultLocationError,
+    defaultLocationErrorMessage: state.locations.defaultLocationErrorMessage
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+	return {
+		actions: bindActionCreators(locationsActions, dispatch)
+	};
+}
+
+class HomeScreen extends Component {
   static navigatorButtons = {
     rightButtons: [
       {
@@ -22,7 +42,23 @@ export default class HomeScreen extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      loading: !props.defaultLocation
+    };
+
     this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent.bind(this));
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.setState({ loading: false }, () => {
+        this.props.actions.setDefaultLocation({ lat: position.coords.latitude, lng: position.coords.longitude });
+      });
+    }, (error) => {
+      this.setState({ loading: false }, () => {
+        this.props.actions.setDefaultLocation({ error: error });
+      });
+    }, { enableHighAccuracy: !__DEV__, timeout: 3000 });
   }
 
   onNavigatorEvent(event) {
@@ -35,11 +71,60 @@ export default class HomeScreen extends Component {
     }
   }
 
+  renderLoading() {
+    if (this.state.loading) {
+      return (
+        <Text>Getting information</Text>
+      );
+    }
+  }
+
+  renderDefaultLocation() {
+    const {
+      defaultLocation,
+      defaultLocationErrorMessage,
+      shouldRequestForGeolocationPermission
+    } = this.props;
+
+    if (defaultLocation) {
+      return (
+        <View>
+          <Text>{ defaultLocation.name }</Text>
+        </View>
+      );
+    } else {
+      if (this.state.loading) {
+        return;
+      }
+
+      return (
+        <View>
+          <Image source={ require('../../assets/icons/sad-cloud.png') } />
+          <Text>{ defaultLocationErrorMessage }</Text>
+        </View>
+      );
+    }
+  }
+
+  renderOtherLocations() {
+    const {
+      otherLocations
+    } = this.props;
+
+    if (otherLocations.length) {
+      return <Text>Other component list</Text>;
+    }
+  }
+
   render() {
     return (
       <View>
-        <Text>HomeScreen</Text>
+        { this.renderLoading() }
+        { this.renderDefaultLocation() }
+        { this.renderOtherLocations() }
       </View>
     );
   }
 }
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeScreen);
